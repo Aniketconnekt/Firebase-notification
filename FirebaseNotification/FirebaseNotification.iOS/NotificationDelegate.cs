53575ce0178f3f1d001b7c55312aeacde96c1159
Aniket Seth <aniket.verma1723@gmail.com>
2020-08-15 10:35:36 +0530

    new commit

---
+++ FirebaseNotification/FirebaseNotification.iOS/NotificationDelegate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using UserNotifications;
+
+namespace FirebaseNotification.iOS
+{
+    class NotificationDelegate : UNUserNotificationCenterDelegate
+    {
+        public NotificationDelegate()
+        {
+
+        }
+        public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+
+        }
+        public override void DidReceiveNotificationResponse(UNUserNotificationCenter center, UNNotificationResponse response, Action completionHandler)
+        {
+
+        }
+        public void RegisterNotification(string title, string body)
+        {
+            UNUserNotificationCenter center = UNUserNotificationCenter.Current;
+
+            //creat a UNMutableNotificationContent which contains your notification content
+            UNMutableNotificationContent notificationContent = new UNMutableNotificationContent();
+
+            notificationContent.Title = title;
+            notificationContent.Body = body;
+
+            notificationContent.Sound = UNNotificationSound.Default;
+
+            UNTimeIntervalNotificationTrigger trigger = UNTimeIntervalNotificationTrigger.CreateTrigger(1, false);
+
+            UNNotificationRequest request = UNNotificationRequest.FromIdentifier("FiveSecond", notificationContent, trigger);
+
+
+            center.AddNotificationRequest(request, (NSError obj) =>
+            {
+
+            });
+        }
+    }
+}
